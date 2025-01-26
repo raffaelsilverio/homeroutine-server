3bd2cc7921b24595ef4e97e43bbfa850d8f92e28
Silverio <raffael.silverio@gmail.com>
2025-01-26 18:25:09 +0000

    Server Structure

---
+++ src/main/kotlin/com/homeroutine/homeroutine/HomeroutineApplication.kt
@@ -0,0 +1,11 @@
+package com.homeroutine.homeroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomeroutineApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomeroutineApplication>(*args)
+}
---
+++ src/test/kotlin/com/homeroutine/homeroutine/HomeroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.homeroutine.homeroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HomeroutineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
